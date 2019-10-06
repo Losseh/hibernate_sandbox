bff3c5fbaac5d72503971477a7367a69e0c7746b
Losseh <aszymanski90@gmail.com>
2019-10-06 22:20:27 +0200

    initial hibernate commit

--- src/main/java/StartClass.java
+++ src/main/java/StartClass.java
@@ -1,15 +1,18 @@
 import model.ExampleEntity;
 import model.HibernateUtil;
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class StartClass {
     public static void main(String[] args)
     {
         System.out.println("Hello world");
 
-        final ExampleEntity ex = new ExampleEntity();
+        final ExampleEntity ex1 = new ExampleEntity("funny data");
+        final ExampleEntity ex2 = new ExampleEntity("another data");
+        final ExampleEntity ex3 = new ExampleEntity("another data3");
 
 
         Transaction transaction = null;
@@ -17,9 +20,23 @@ public class StartClass {
             // start a transaction
             transaction = session.beginTransaction();
             // save the student objects
-            session.save(ex);
+            session.save(ex1);
+            session.save(ex2);
+            session.save(ex3);
             // commit transaction
             transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        System.out.println("put data into the DB. Trying to read");
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            final List<ExampleEntity> students = session.createQuery("from ExampleEntity", ExampleEntity.class).list();
+            students.forEach(x -> System.out.println(x.getData()));
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
--- src/main/java/model/ExampleEntity.java
+++ src/main/java/model/ExampleEntity.java
@@ -3,20 +3,30 @@ package model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "example_entity")
+@Table(name = "example")
 public class ExampleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "data")
+    private String data;
 
+    // it must exist for hibernate
     public ExampleEntity()
-    {
+    {}
 
+    public ExampleEntity(final String data)
+    {
+        this.data = data;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getData() {
+        return data;
+    }
 }
